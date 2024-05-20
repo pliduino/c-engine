9b8b4f389d2cb684cd9197497a7888c52b2ee990
pliduino <liduinopedro@gmail.com>
2024-05-20 20:26:17 -0300

    fixed build

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 To build simply run this command inside the aurora-engine folder
 
 ```
+  bdep init -C ../aurora-compiler-clang @clang cc config.cxx=clang++
   b config.log-verbosity=5
 ```
+
+The first one creates the configuration files for clang, if you want to feel free to use gcc instead, just change @clang to @gcc and clang++ to g++
 
 NOTE: You need to install build2 to build this project
--- aurora-engine/Pointers/Pointers.h
+++ aurora-engine/Pointers/Pointers.h
@@ -2,73 +2,56 @@
 
 // Helper types only, Borrows shall not be freed, Owners shall always be freed
 
-template <class T>
-using Owner = T *;
+template <class T> using Owner = T *;
 
-template <class T>
-using Borrow = T *;
+template <class T> using Borrow = T *;
 
-template <typename T>
-class TSharedPointer
-{
-    T *Ptr;
-    unsigned int *RefCount;
+template <typename T> class TSharedPointer {
+  T *Ptr;
+  unsigned int *RefCount;
 
 public:
-    TSharedPointer(T *Asset)
-    {
-        this->Ptr = Asset;
-        RefCount = new uint(0);
-    }
+  TSharedPointer(T *Asset) {
+    this->Ptr = Asset;
+    RefCount = new uint(0);
+  }
 
-    ~TSharedPointer()
-    {
-        (*RefCount)--;
-        if (*RefCount == 0)
-        {
-            delete Ptr;
-            delete RefCount;
-        }
+  ~TSharedPointer() {
+    (*RefCount)--;
+    if (*RefCount == 0) {
+      delete Ptr;
+      delete RefCount;
     }
+  }
 
-    TSharedPointer(const TSharedPointer &other)
-    {
-        this->Ptr = other.Ptr;
-        this->RefCount = other.RefCount;
+  TSharedPointer(const TSharedPointer &other) {
+    this->Ptr = other.Ptr;
+    this->RefCount = other.RefCount;
 
-        (*this->RefCount)++;
-    }
+    (*this->RefCount)++;
+  }
 
-    TSharedPointer &operator=(const TSharedPointer &other)
-    {
-        this->ptr = other.ptr;
-        this->RefCount = other.RefCount;
+  TSharedPointer &operator=(const TSharedPointer &other) {
+    this->ptr = other.ptr;
+    this->RefCount = other.RefCount;
 
-        (*this->RefCount)++;
-    }
+    (*this->RefCount)++;
+  }
 
-    TSharedPointer(TSharedPointer &&other)
-    {
-        this->Ptr = other.Ptr;
-        this->RefCount = other.refCoRefCountunt;
-        other.Ptr = nullptr;
-        other.RefCount = nullptr;
-    }
+  TSharedPointer(TSharedPointer &&other) {
+    this->Ptr = other.Ptr;
+    this->RefCount = other.refCoRefCountunt;
+    other.Ptr = nullptr;
+    other.RefCount = nullptr;
+  }
 
-    TSharedPointer &operator=(TSharedPointer &&other)
-    {
-        this->Ptr = other.Ptr;
-        this->RefCount = other.refCoRefCountunt;
-        other.Ptr = nullptr;
-        other.RefCount = nullptr;
-    }
+  TSharedPointer &operator=(TSharedPointer &&other) {
+    this->Ptr = other.Ptr;
+    this->RefCount = other.refCoRefCountunt;
+    other.Ptr = nullptr;
+    other.RefCount = nullptr;
+  }
 
-    T *operator->() const
-    {
-        return this->Ptr;
-    }
-    T &operator*() const
-    {
-        return this->Ptr;
-    }
+  T *operator->() const { return this->Ptr; }
+  T &operator*() const { return this->Ptr; }
 };
